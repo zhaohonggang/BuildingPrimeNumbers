1d6204f87b1eb5bd97e9a27875a5f46c189870ac
Honggang(Grant). Zhao <9408982+zhaohonggang@users.noreply.github.com>
2017-11-22 22:48:58 -0500

    added math

--- primepcn.py
+++ primepcn.py
@@ -1,63 +1,79 @@
+'''
+Theory:
+
+P - Prime Numbers: P = {p(n)|n>=1,p(n) is the n-th prime number}
+C - Prefect Composite Number: C = {c(p(n))| c(p(n)) = *p(i)|1<=i<=p(n)}
+NC - Numbers co-prime with C: NC(c(p(n))) = {nc(c(p(n)))| nc(c(p(n))) is co-prime with c(p(n))}
+
+NC{ c(p(n))..c(p(n+1)) } - Co-prime List. They are all possible numbers between c(p(n))and c(p(n+1)) which is co-prime with c(p(n)):
+
+    NC{ c(p(n))..c(p(n+1)) } = {nc(c(p(n)))| c(p(n)) < nc(c(p(n))) < c(p(n+1)), nc(c(p(n))) is co-prime with c(p(n))}
+                             = {c(p(n)) * k + s(c(p(n)))| 0<k<p(n+1), 0<s(c(p(n)))<c(p(n)), s(c(p(n))) is co-prime with c(p(n)) }
+                             = {c(p(n)) * k + s(c(p(n)))| 0<k<p(n+1), 0<s(c(p(n)))<c(p(n)), s(c(p(n))) = *p(i)|p(n+1)<=p(i)<c(p(n)) }
+
+NCC{ c(p(n))..c(p(n+1)) } - Except List. They are all possible composite numbers between c(p(n))and c(p(n+1)) which is co-prime with c(p(n)):
+
+    NCC{ c(p(n))..c(p(n+1)) } = {ncc(c(p(n)))| c(p(n)) < ncc(c(p(n))) < c(p(n+1)), ncc(c(p(n))) is co-prime with c(p(n)), ncc(c(p(n))) is composite number }
+                              = {ncc(c(p(n)))| c(p(n)) < ncc(c(p(n))) < c(p(n+1)), ncc(c(p(n))) = *p(i)|p(n+1)<=p(i)<c(p(n))}
+
+Finally,
+P{ c(p(n))..c(p(n+1)) } - Primes List. They are all possible prime numbers between c(p(n))and c(p(n+1)) which is co-prime with c(p(n)):
+
+    P{ c(p(n))..c(p(n+1)) } = NC{ c(p(n))..c(p(n+1)) } - NCC{ c(p(n))..c(p(n+1)) }
+
+That's all
+'''
 from math import log
 '''
-from primepcn import primepcn
-primepcn(5)
-a = primepcn(10)
+from primepcn import getPrimes
+a = getPrimes(6)
 '''
-def primepcn(level):
-    # compare to v1, it is simpler and clearer describe the theory. Faster than v1 now
+def getPrimes(level):
     """primepcn(level): return a list of all the prime numbers using Prefect Composite Number Theory founded by Honggang(Grant), Zhao from 2017-11-11"""
     # parameter is the level of profect composite number 1|{1}, 2|{2}, 3|{6}, 4|{30} 5|{210}, 6|2310,7:30030,8:510510,9:9699690,10:223092870
     # Code by Honggang(Grant), Zhao, <honggangz@gmail.com>, from 2017-11-16
-    # Code: https://github.com/zhaohonggang/BuildingPrimeNumbers/
+    # Code: https://github.com/zhaohonggang/BuildingPrimeNumbers/primepcn.py
     # Info: https://github.com/zhaohonggang/BuildingPrimeNumbers/blob/master/building%20prime%20number.txt
-    # Math: https://github.com/zhaohonggang/BuildingPrimeNumbers/blob/master/prime%20algorithm.txt
 
     c = 1 # current Prefect Composite Number
     c_n = 2 # next Prefect Composite Number
-    p_l = [] #Prime number lisk
-    p_n = 2 #Next Prime number
+    p_l = [] # Prime number list
+    p_n = 2 # Next Prime number
 
     for n in xrange(1,level):
         print "min {} max {}".format(c, c_n)
 
-        # Prepare Composite Number validation List will apprear in the generation
-        v = [] if n-1 >= len(p_l) else list(set(array_product_all_nr(p_l[n-1:], c, c_n)))
+        # Prepare Composite Number Except List will appear in the generation
+        e = [] if n-1 >= len(p_l) else list(set(getArrayProducts(p_l[n-1:], c, c_n)))
+        print "Except List Prepared:{}"#.format(v)
 
-        print "validation List Prepared:{}"#.format(v)
+        # Sort the Except list to be used in the next stage
+        e.sort()
+        print "Except List Sorted"
 
-        # Sort the validation list to be used in the next stage
-        v.sort()
-        print "validation List Sorted"
-
-        s = [1] if n-1 >= len(p_l) else list(set(array_product_all_nr_and_one(p_l[n-1:], 2, c)))
-
-        print "seed List Prepared:{}"#.format(s)
+        s = [1] + ([] if n-1 >= len(p_l) else list(set(getArrayProducts(p_l[n-1:], 2, c))))
+        print "Seed List Prepared:{}"#.format(s)
 
         # Sort the validation list to be used in the next stage
         s.sort()
-        print "seed List Sorted"
+        print "Seed List Sorted"
 
-        v_i = 0
+        e_i = 0 # Except List Index
         # generate prime numbers based on the Prefect Composite Number and the Co-prime list
         for k in xrange(1, p_n):
             for i in s:
                 t = c * k + i
 
-                # prime validation
-                if len(v) > v_i:
-                    if t != v[v_i]:
+                # prime validation. If t is not in Except List, t is prime.
+                if len(e) > e_i:
+                    if t != e[e_i]:
                         p_l.append(t)
                     else:
-                        v_i = v_i + 1
+                        e_i = e_i + 1
                 else:
                     p_l.append(t)
 
         print "primes generated"
-
-        # exit door
-        if n == level - 1:
-            break;
 
         if n == 1:
             p_n = 3
@@ -67,10 +83,101 @@ def primepcn(level):
         c = c_n
         c_n = c * p_n
 
+        del e[:]
+        del s[:]
         print "ready for the next round"
 
     print 'count:{}\n'.format(len(p_l))
     return p_l
+
+'''
+a = [7, 11, 13]
+b = list(getArrayProducts([5], 6, 30))
+from primepcn import getArrayProducts
+'''
+def getArrayProducts(array, min, max):
+    # multiply sorted array numbers any times including 0,1,2..n.., yield results between min and max
+    # Code by Honggang(Grant), Zhao, <honggangz@gmail.com>
+    if len(array) == 0:
+        return
+
+    array_0 = array[0]
+
+    max_1 = float(max) / array_0
+
+    max_level = int(log(max, array[0])) - 1
+
+    min_level = int(log(min, array[-1]))
+
+    t_max = max / (array_0 ** min_level)
+
+    max_i = 0
+
+    while max_i < len(array) and array[max_i] < t_max:
+        max_i = max_i + 1
+
+    #print "array:{},min:{},max:{},max_i:{},t_max:{}".format(array, min, max, max_i, t_max)
+
+    l = 0
+    stack = []
+
+    if min_level == 0:# push or yield
+        for i in xrange(0, max_i):
+            t = array[i]
+            if t > max:
+                break
+            stack.append(t)
+            if min < t:
+                yield t
+    else:# push only
+        for i in xrange(0, max_i):
+            t = array[i]
+            if t > max:
+                break
+            stack.append(t)
+
+    while l <= max_level - 1:
+        #print stack
+        stack_n = []
+        if l < min_level - 1: # push only
+            for j in xrange(0, len(stack)):
+                a = stack[j]
+                for i in xrange(0, max_i):
+                    if array[i] > a:
+                        break
+                    t = a * array[i]
+                    if t > max:
+                        break
+                    if t < max_1:
+                        stack_n.append(t)
+        elif l == max_level - 1: # yield only
+            for j in xrange(0, len(stack)):
+                a = stack[j]
+                for i in xrange(0, max_i):
+                    if array[i] > a:
+                        break
+                    t = a * array[i]
+                    if t > max:
+                        break
+                    if min < t:
+                        yield t
+        else: # push or yield
+            for j in xrange(0, len(stack)):
+                a = stack[j]
+                for i in xrange(0, max_i):
+                    if array[i] > a:
+                        break
+                    t = a * array[i]
+                    if t > max:
+                        break
+                    if min < t:
+                        yield t
+                    if t < max_1:
+                        stack_n.append(t)
+
+        l = l + 1
+        del stack[:]
+        stack = stack_n
 
 '''
 from primepcn import primepcn_v1
@@ -78,12 +185,12 @@ primepcn_v1(5)
 a = primepcn_v1(10)
 '''
 def primepcn_v1(level):
-    """primepcn(level): return a list of all the prime numbers using Prefect Composite Number Theory founded by Honggang(Grant), Zhao from 2017-11-11"""
+    # it is the first version of primepcn. n_c_l is accumulating in this version.
+    """primepcn_v1(level): return a list of all the prime numbers using Prefect Composite Number Theory founded by Honggang(Grant), Zhao from 2017-11-11"""
     # parameter is the level of profect composite number 1|{1}, 2|{2}, 3|{6}, 4|{30} 5|{210}, 6|2310,7:30030,8:510510,9:9699690,10:223092870
     # Code by Honggang(Grant), Zhao, <honggangz@gmail.com>, from 2017-11-16
-    # Code: https://github.com/zhaohonggang/BuildingPrimeNumbers/
+    # Code: https://github.com/zhaohonggang/BuildingPrimeNumbers/primepcn.py
     # Info: https://github.com/zhaohonggang/BuildingPrimeNumbers/blob/master/building%20prime%20number.txt
-    # Math: https://github.com/zhaohonggang/BuildingPrimeNumbers/blob/master/prime%20algorithm.txt
 
     c = 1 # current Prefect Composite Number
     c_n = 2 # next Prefect Composite Number
@@ -95,7 +202,7 @@ def primepcn_v1(level):
         print "min {} max {}".format(c, c_n)
 
         # Prepare Composite Number validation List will apprear in the generation
-        v = list(set(array_product_2(n_c_l[2:], c, c_n)))
+        v = list(set(get_array_product_once(n_c_l[2:], c, c_n)))
         print "validation List Prepared"
 
         # Sort the validation list to be used in the next stage
@@ -151,7 +258,7 @@ def primepcn_v1(level):
     print 'count:{}\n'.format(len(p_l))
     return p_l
 
-def array_product_2(array, min, max):
+def get_array_product_once(array, min, max):
     # multiply sorted array numbers one time, yield results between min and max
     # Code by Honggang(Grant), Zhao, <honggangz@gmail.com>
     l = len(array)
@@ -179,74 +286,8 @@ def array_product_2(array, min, max):
             if t > max:
                 break
 
-def array_product_all_nr_and_one(array, min, max):
-    # multiply sorted array numbers for any possible ways, yield results between min and max
-    # Code by Honggang(Grant), Zhao, <honggangz@gmail.com>
-    yield 1
-    for a in array_product_all_nr(array, min, max):
-        yield a
-
-'''
-a = [7, 11, 13]
-b = list(array_product_all_nr([5], 6, 30))
-from primepcn import array_product_all_nr
-'''
-def array_product_all_nr(array, min, max):
-    if len(array) == 0:
-        return
-
-    array_0 = array[0]
-
-    max_level = int(log(max, array[0])) - 1
-
-    min_level = int(log(min, array[-1])) - 1
-
-    if min_level < 0:
-        min_level = 0
-
-    t_max = max ** (1.0 / (min_level + 1))
-
-    max_i = 0
-
-    while max_i < len(array) and array[max_i] < t_max:
-        max_i = max_i + 1
-
-    #print "array:{},min:{},max:{},max_i:{}".format(array, min, max, max_i)
-
-    l = 0
-    stack = []
-    for i in xrange(0, max_i):
-        t = array[i]
-        if t > max:
-            break
-        if min < t < max:
-            yield t
-        if t * array_0 < max:
-            stack.append(t)
-    len_p = 0
-
-    while l <= max_level - 1:
-        #print stack
-        len_n = len(stack)
-        for j in xrange(len_p, len_n):
-            a = stack[j]
-            for i in xrange(0, max_i):
-                if array[i] > a:
-                    break
-                t = a * array[i]
-                if t > max:
-                    break
-                if min < t < max:
-                    yield t
-                if l < max_level - 1 and t * array_0 < max:
-                    stack.append(t)
-
-        l = l + 1
-        len_p = len_n
 
 
         
 if __name__ == '__main__':
-    b = list(array_product_all([7, 11, 13], 0, 200))
-
-    primepcn(5)
+    b = list(getArrayProducts([5], 6, 30))
